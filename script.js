6eda38c26ccc4175111a4d63770d995d540fefc6
faizsheikh1608 <164844564+faizsheikh1608@users.noreply.github.com>
2024-12-29 12:08:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', () => {
+        let soundFile = button.getAttribute('data-sound');
+        playSound(soundFile);
+    });
+});
+
+document.querySelector('.stop').addEventListener('click', () => {
+    stopSound();
+});
+
+let audio = new Audio();
+
+function playSound(soundFile) {
+    if (audio.src !== soundFile) {
+        audio.src = `sounds/${soundFile}`;
+        audio.play();
+    } else {
+        audio.play();
+    }
+}
+
+function stopSound() {
+    audio.pause();
+    audio.currentTime = 0; // Reset to the start of the sound
+}
